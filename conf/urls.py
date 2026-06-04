e6448cb82478e705bafc6561819bcd5c67a54124
ESapenaVentura <sapenaventura@gmail.com>
2026-06-04 11:12:11 +0200

    Fixed schema and swagger to be served from version

--- conf/urls.py
+++ conf/urls.py
@@ -3,12 +3,7 @@ from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="swagger-ui", permanent=False)),
-    path("openapi/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "swagger/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path("v1/", include("core.api.v1.urls")),
+    path("", RedirectView.as_view(url="v1/", permanent=False)),
+    path("swagger/", RedirectView.as_view(url="/v1/swagger/", permanent=False)),
+    path("v1/", include("core.api.v1.urls", namespace="v1")),
 ]
